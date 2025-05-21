0c5d1ff49f8ed52ada7246894e60aadb2ea924c4
Alex-Alessi <alex.alessi246@gmail.com>
2025-05-21 17:34:47 +0200

    milestone 3

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+const API_URL = "http://localhost:3333";
+
 type Person = {
   readonly id: number;
   readonly name: string;
@@ -6,6 +8,48 @@ type Person = {
   biography: string;
   image: string;
 };
+
+function isActress(dati: unknown): dati is Actress {
+  if (
+    typeof dati === "object" &&
+    dati !== null &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    "death_year" in dati &&
+    typeof dati.death_year === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "most_famous_movies" in dati &&
+    dati.most_famous_movies instanceof Array &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every((m) => typeof m === "string") &&
+    "awards" in dati &&
+    typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string" &&
+    [
+      "British",
+      "Australian",
+      "Israeli-American",
+      "South African",
+      "French",
+      "Indian",
+      "Israeli",
+      "Spanish",
+      "South Korean",
+      "Chinese",
+    ].includes(dati.nationality)
+  ) {
+    return true;
+  }
+  return false;
+}
 
 type Actress = Person & {
   most_famous_movies: [string, string, string];
@@ -23,3 +67,23 @@ type Actress = Person & {
     | "South Korean"
     | "Chinese";
 };
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`${API_URL}/actresses/${id}`);
+    const dati: unknown = await response.json();
+    if (!response.ok) {
+      throw new Error(
+        "Errore HTTP" + response.status + " :" + response.statusText
+      );
+    }
+
+    if (!isActress(dati)) {
+      throw new Error("Non è un'attrice");
+    }
+    return dati;
+  } catch (errore) {
+    console.error(errore);
+    return null;
+  }
+}
